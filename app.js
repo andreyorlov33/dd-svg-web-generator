2caf0529b60c02ffaaa749797a9a8fb63319e48b
UV <uv@INTAKE.local>
2018-01-18 14:01:45 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+import fs from 'fs'
+import inquirer from 'inquirer'
+import chokidar from 'chokidar'
+const cli = inquirer.createPromptModule()
+
+class App {
+    constructor(props){
+        this.generating = false
+    }
+
+    start(){
+        
+    }
+}
